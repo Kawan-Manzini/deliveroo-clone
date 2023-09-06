85b5c5640b43f2173212cbc5943797520c055615
Kawan Manzini <kawan.manzini@qodeless.io>
2023-09-06 14:58:36 -0300

    config settings

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View } from 'react-native';
 import { TailwindProvider } from 'tailwindcss-react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './screens/HomeScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -12,10 +13,7 @@ export default function App() {
     <NavigationContainer>
       <TailwindProvider>
         <Stack.Navigator>
-          {/* <View >
-          <Text className='items-center content-center text-red-500'>Open up App.js to start working on your app!</Text>
-          <StatusBar style="auto" />
-        </View> */}
+          <Stack.Screen name="Home" component={HomeScreen} />
         </Stack.Navigator>
       </TailwindProvider>
     </NavigationContainer>
